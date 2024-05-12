5ebb28249557ea0df3168cbe6b7732b35e38411a
HamzaSourani <hamzasourani98@gmail.com>
2024-05-12 21:34:07 +0400

    add video element

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function HomePage() {
-  return <div></div>;
+  return (
+    <div>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className=" h-[60vh] md:h-screen w-screen object-cover"
+      >
+        <source
+          src={
+            "https://dud8cevzzy9qg.cloudfront.net/videos/BRAND_VIDEO_GENERIC_16X9_APPROVED-LOWRES.mp4"
+          }
+          type="video/mp4"
+        />
+      </video>
+    </div>
+  );
 }
